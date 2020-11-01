edc85faa62576d4da645b8c2d88b61c9fd2e9f7f
Yicha <1114637844@qq.com>
2020-11-02 00:41:36 +0800

    完成发布页面

--- pages/index/index.js
+++ pages/index/index.js
@@ -15,39 +15,11 @@ Page({
     })
   },
   onLoad: function () {
-    const token = getToken();
-    if(token) {
-      // 获取用户信息
-
-      if (app.globalData.userInfo) {
-        this.setData({
-          userInfo: app.globalData.userInfo,
-          hasUserInfo: true
-        })
-      } else if (this.data.canIUse){
-        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-        // 所以此处加入 callback 以防止这种情况
-        app.userInfoReadyCallback = res => {
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      } else {
-        // 在没有 open-type=getUserInfo 版本的兼容处理
-        wx.getUserInfo({
-          success: res => {
-            app.globalData.userInfo = res.userInfo
-            this.setData({
-              userInfo: res.userInfo,
-              hasUserInfo: true
-            })
-          }
-        })
-      }
-   
-    }
-    //  else {
+    // const token = getToken(app.globalData.token);
+    // if(token) {
+    //   // 获取用户信息
+    //   this.getUserInfo()
+    // } else {
     //   // 跳转到登录页面
     //   wx.redirectTo({
     //     url: '../login/login'
--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -1,21 +1,43 @@
-// pages/issue/issue.js
+import { postAction, getAction } from '../../api/requests.js'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    url: '',
+    issue: {
+      inputTxt: '',
+      fee: '',
+      startDate: 0,
+      endDate: 0,
+      origin: '',
+      destination: ''
+    },
     total: 0, // 已输入的总字数
-    chooesTime: false,
-    // currentTime: new Date(),
-    // currentDate: currentTime.getHour() + ':' + currentTime.getHour(),
+    startDate: 0, // 开始时间
+    endDate: 0, // 结束时间 
+    choosePicker: false,
+    choosePickerId: 0, // 0：开始时间选择器 1：结束时间选择器
+    scale: 3600000, // 默认时间跨度为一小时
+    minDate: 0,
+    maxDate: 0,
+    currentDate: new Date().getTime()
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      startDate: this.formatDate(this.data.currentDate),
+      endDate: this.formatDate(this.data.currentDate + this.data.scale), // 默认结束时间为当前时间一小时后
+      minDate: this.data.currentDate,
+      maxDate: this.data.currentDate + this.data.scale * 24 * 7 // 最大时间为当前时间的一周之后
+    })
+    this.data.issue.startDate = this.data.currentDate
+    this.data.issue.endDate = this.data.currentDate
   },
 
   /**
@@ -66,28 +88,87 @@ Page({
   onShareAppMessage: function () {
 
   },
+  modifyDate: function(date) {
+    return date < 10 ? '0' + date : date 
+  },
+  formatDate: function(date) {
+    const formatDate = new Date(date)
+    const m = formatDate.getMonth() + 1
+    const d = formatDate.getDate()
+    const h = formatDate.getHours()
+    const mm = formatDate.getMinutes()
+    return this.modifyDate(m) + '-' + this.modifyDate(d) + ' ' + this.modifyDate(h) + ':' + this.modifyDate(mm)
+  },
   inputText: function(e) {
     const len = e.detail.value.length
     this.setData({
       total: len
     })
+    this.data.issue.inputTxt = e.detail.value
   },
-  showPicker: function() {
+  feeBlur: function(e) {
+    this.data.issue.fee = e.detail.value
+  }, 
+  originBlur: function(e) {
+    this.data.issue.origin = e.detail.value
+  },
+  detinationBlur: function(e) {
+    this.data.issue.destination = e.detail.value
+  },
+  showPicker: function(e) {
     this.setData({
-      chooesTime: true
+      choosePicker: true
     })
+    this.data.choosePickerId = e.currentTarget.dataset.value === 0 ? 0 : 1
   },
-  onInput: function(event) {
+  handleConfirm: function(e) {
     this.setData({
-      currentDate: event.detail,
-    });
-  },
-  handleConfirm: function() {
-    
+      choosePicker: false
+    })
+    if(this.data.choosePickerId === 0) {
+      this.setData({
+        startDate: this.formatDate(e.detail)
+      })
+      this.data.issue.startDate = e.detail
+    } else {
+      this.setData({
+        endDate: this.formatDate(e.detail)
+      })
+      this.data.issue.endDate = e.detail
+    }
   },
   handleCancel: function() {
     this.setData({
-      chooesTime: false
+      choosePicker: false
     })
+  },
+  send: function() {
+    let info = ''
+    if(this.data.issue.inputTxt === '') {
+      info = '描述不能为空'
+    } else if(this.data.issue.fee === '') {
+      info = '费用不能为空'
+    } else if(this.data.issue.origin) {
+      info = '起点不能为空'
+    } else if(this.data.issue.destination) {
+      info = '终点不能为空'
+    } else if(this.data.issue.endDate < this.data.issue.startDate) {
+      info = '结束时间不能小于开始时间'
+    }
+    if(info !== '') {
+      wx.showToast({
+        title: info,
+        icon: 'none',
+        duration: 2000
+      })
+    } else {
+      postAction(this.data.url, this.data.issue)
+      .then(res => {
+        console.log('发布成功', res)
+      })
+      .catch(err => {
+        console.log('发布失败', err)
+      })
+    }
   }
 })
