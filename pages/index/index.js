65b126e0612a2865b26a35680188919d5b3f083a
HUAHUAXIA <1790453161@qq.com>
2020-11-01 15:57:17 +0800

    [feat] 添加搜索结果为无的界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,8 @@
 //获取应用实例
 const app = getApp()
-import { getToken } from '../../utils/cookies.js'
+import {
+  getToken
+} from '../../utils/cookies.js'
 var tabar = require('../../templates/tabar/tabar.js');
 Page({
   data: {
@@ -8,31 +10,49 @@ Page({
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     show: false, // 控制遮罩层的显示与隐藏
-    sortRules: [
-      { text: '最新发布', value: '0' },
-      { text: '发布最久', value: '1' },
-      { text: '价格升序', value: '2' },
-      { text: '价格降序', value: '3' },
-      { text: '距离最近', value: '4' },
-      { text: '距离最远', value: '5' },
+    initIcon: true, // 控制图标颜色
+    sortRules: [{
+        text: '最新发布',
+        value: '0'
+      },
+      {
+        text: '发布最久',
+        value: '1'
+      },
+      {
+        text: '价格升序',
+        value: '2'
+      },
+      {
+        text: '价格降序',
+        value: '3'
+      },
+      {
+        text: '距离最近',
+        value: '4'
+      },
+      {
+        text: '距离最远',
+        value: '5'
+      },
     ],
     serachValue: '', // 搜索关键字
-    orderList: [], // 订单列表
+    orderList: [{value:1}], // 订单列表
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../index/index'
     })
   },
   onLoad: function () {
-    tabar.tabbar("tabBar", 0, this)//0表示第一个tabbar
+    tabar.tabbar("tabBar", 0, this) //0表示第一个tabbar
     this.setData({ // 将暂存在全局的搜索关键字保存在searchValue中
       serachValue: app.globalData.serachKey,
     })
     console.log('我是首页的搜索关键字：' + this.data.serachValue)
     const token = getToken();
-    if(token) {
+    if (token) {
       // 获取用户信息
 
       if (app.globalData.userInfo) {
@@ -40,7 +60,7 @@ Page({
           userInfo: app.globalData.userInfo,
           hasUserInfo: true
         })
-      } else if (this.data.canIUse){
+      } else if (this.data.canIUse) {
         // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
         // 所以此处加入 callback 以防止这种情况
         app.userInfoReadyCallback = res => {
@@ -61,7 +81,7 @@ Page({
           }
         })
       }
-   
+
     }
     //  else {
     //   // 跳转到登录页面
@@ -71,7 +91,7 @@ Page({
     // }
   },
   // 获取用户信息
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -80,32 +100,42 @@ Page({
     })
   },
   onClickShow() {
-    this.setData({ show: true });
-    console.log('遮罩层出现')
+    this.setData({
+      show: true,
+      initIcon: false
+    });
+    console.log('遮罩层出现,图标变色')
   },
 
   onClickHide() {
-    this.setData({ show: false });
+    this.setData({
+      show: false,
+      initIcon: true
+    });
     console.log('遮罩层隐藏')
 
   },
   noop() {},
   // 改变排序方式的方法
   changeSortRule(e) {
+    this.setData({
+      show: false,
+      initIcon: true
+    })
     // 点击的排序方式索引
     console.log(e.target.dataset.value)
   },
   // 点击搜索框导航至搜索页面
-  toSearch(){
+  toSearch() {
     wx.navigateTo({
       url: '/pages/serach/serach',
     })
   },
   // 从搜索页面回来的真正搜索
-  toGetSearchList(){
-    if(this.data.serachValue){
+  toGetSearchList() {
+    if (this.data.serachValue) {
       // 如果搜索关键字不为空，发起获取订单列表的请求
-    }else{
+    } else {
       // 如果关键字为空，获取全部订单
     }
   }
