70e8a0106a02593e577ff7167e0a15f3caa2ccb1
HUAHUAXIA <1790453161@qq.com>
2020-11-09 00:43:24 +0800

    [feat] 我的 静态页面初步开发

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -4,10 +4,23 @@ import {
   getToken
 } from '../../utils/cookies.js'
 Page({
-  data: {},
+  data: {
+    tabActive: 2,
+    num: 5,
+    order: {
+      userName: '花花',
+      createTime: '3分钟前',
+      orderContent: '球带一份鸡肉卷球带一份鸡肉卷球带一份鸡肉卷',
+      address1: '六饭',
+      address2: '东十三',
+      time1: '23:20',
+      time2: '23:40',
+      money: '5'
+    },
+  },
   onLoad: function () {
-    tabar.tabbar("tabBar", 1, this), //1表示第二个tabbar
-      this.checkUserLogin()
+    tabar.tabbar("tabBar", 1, this) //1表示第二个tabbar
+      // this.checkUserLogin()
   },
   // 判断用户是否已登陆
   checkUserLogin() {
@@ -32,6 +45,8 @@ Page({
       })
       return
     }
-  }
-
+  },
+  tagChange(e) {
+    console.log(`切换到标签 ${e.detail.name}`)
+  },
 })
