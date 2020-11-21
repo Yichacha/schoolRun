866cfddcb0a8bb65d8c7b8e1619ebed3a22c9c5c
Yicha <1114637844@qq.com>
2020-11-21 17:57:24 +0800

    [add] 实现发布功能

--- pages/issue/issue.js
+++ pages/issue/issue.js
@@ -1,4 +1,8 @@
-import { postAction, getAction } from '../../api/requests.js'
+import { getToken, setToken, removeToken } from '../../utils/cookies.js'
+import { sendApi } from '../../api/api.js'
+import { formatTimeTwo } from '../../utils/util.js'
+
+const app = getApp()
 
 Page({
 
@@ -6,18 +10,17 @@ Page({
    * 页面的初始数据
    */
   data: {
-    url: '',
     issue: {
-      inputTxt: '',
-      fee: '',
-      startDate: 0,
-      endDate: 0,
-      origin: '',
+      content: '',
+      exceptedCost: '',
+      expectStartTimeString: 0,
+      expectEndTimeString: 0,
+      startingPoint: '',
       destination: ''
     },
     total: 0, // 已输入的总字数
-    startDate: 0, // 开始时间
-    endDate: 0, // 结束时间 
+    expectStartTimeString: 0, // 开始时间
+    expectEndTimeString: 0, // 结束时间 
     choosePicker: false,
     choosePickerId: 0, // 0：开始时间选择器 1：结束时间选择器
     scale: 3600000, // 默认时间跨度为一小时
@@ -31,13 +34,13 @@ Page({
    */
   onLoad: function (options) {
     this.setData({
-      startDate: this.formatDate(this.data.currentDate),
-      endDate: this.formatDate(this.data.currentDate + this.data.scale), // 默认结束时间为当前时间一小时后
+      expectStartTimeString: formatTimeTwo(this.data.currentDate, 'M-D h:m'),
+      expectEndTimeString: formatTimeTwo(this.data.currentDate + this.data.scale, 'M-D h:m'), // 默认结束时间为当前时间一小时后
       minDate: this.data.currentDate,
       maxDate: this.data.currentDate + this.data.scale * 24 * 7 // 最大时间为当前时间的一周之后
     })
-    this.data.issue.startDate = this.data.currentDate
-    this.data.issue.endDate = this.data.currentDate
+    this.data.issue.expectStartTimeString = formatTimeTwo(this.data.currentDate, 'Y-M-D h:m:s')
+    this.data.issue.expectEndTimeString = formatTimeTwo(this.data.currentDate + this.data.scale, 'Y-M-D h:m:s')
   },
 
   /**
@@ -54,63 +57,18 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-  modifyDate: function(date) {
-    return date < 10 ? '0' + date : date 
-  },
-  formatDate: function(date) {
-    const formatDate = new Date(date)
-    const m = formatDate.getMonth() + 1
-    const d = formatDate.getDate()
-    const h = formatDate.getHours()
-    const mm = formatDate.getMinutes()
-    return this.modifyDate(m) + '-' + this.modifyDate(d) + ' ' + this.modifyDate(h) + ':' + this.modifyDate(mm)
-  },
   inputText: function(e) {
     const len = e.detail.value.length
     this.setData({
       total: len
     })
-    this.data.issue.inputTxt = e.detail.value
+    this.data.issue.content = e.detail.value
   },
   feeBlur: function(e) {
-    this.data.issue.fee = e.detail.value
+    this.data.issue.exceptedCost = Number(e.detail.value)
   }, 
   originBlur: function(e) {
-    this.data.issue.origin = e.detail.value
+    this.data.issue.startingPoint = e.detail.value
   },
   detinationBlur: function(e) {
     this.data.issue.destination = e.detail.value
@@ -127,14 +85,14 @@ Page({
     })
     if(this.data.choosePickerId === 0) {
       this.setData({
-        startDate: this.formatDate(e.detail)
+        expectStartTimeString: formatTimeTwo(e.detail, 'M-D h:m')
       })
-      this.data.issue.startDate = e.detail
+      this.data.issue.expectStartTimeString = formatTimeTwo(e.detail, 'Y-M-D h:m:s')
     } else {
       this.setData({
-        endDate: this.formatDate(e.detail)
+        expectEndTimeString: ormatTimeTwo(e.detail, 'M-D h:m')
       })
-      this.data.issue.endDate = e.detail
+      this.data.issue.expectEndTimeString = formatTimeTwo(e.detail, 'Y-M-D h:m:s')
     }
   },
   handleCancel: function() {
@@ -142,17 +100,17 @@ Page({
       choosePicker: false
     })
   },
-  send: function() {
+  async send() {
     let info = ''
-    if(this.data.issue.inputTxt === '') {
+    if(this.data.issue.content === '') {
       info = '描述不能为空'
-    } else if(this.data.issue.fee === '') {
+    } else if(this.data.issue.exceptedCost === '') {
       info = '费用不能为空'
-    } else if(this.data.issue.origin) {
+    } else if(this.data.issue.startingPoint === '') {
       info = '起点不能为空'
-    } else if(this.data.issue.destination) {
+    } else if(this.data.issue.destination === '') {
       info = '终点不能为空'
-    } else if(this.data.issue.endDate < this.data.issue.startDate) {
+    } else if(this.data.issue.expectEndTimeString < this.data.issue.expectStartTimeString) {
       info = '结束时间不能小于开始时间'
     }
     if(info !== '') {
@@ -162,13 +120,26 @@ Page({
         duration: 2000
       })
     } else {
-      postAction(this.data.url, this.data.issue)
-      .then(res => {
-        console.log('发布成功', res)
-      })
-      .catch(err => {
-        console.log('发布失败', err)
-      })
+      let header = {
+        'Authorization': getToken(app.globalData.token),
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+      const { data: res } = await sendApi(this.data.issue, header)
+      console.log('发布跑腿信息', res)
+      if(res.code === 1) {
+        wx.showToast({
+          title: res.message,
+          icon: 'success'
+        })
+        wx.redirectTo({
+          url: '../index/index',
+        })
+      } else {
+        wx.showToast({
+          title: '发送失败，请稍后重试',
+          icon: 'none'
+        })
+      }
     }
   }
 })
